ac3e360726483dffc0047a8780ee1a87fd14152a
andrewfrideres13 <andrewfrideres13@gmail.com>
2019-02-13 18:15:06 -0600

    Signed-off-by: andrewfrideres13 <andrewfrideres13@gmail.com>

--- js/gVenture.js
+++ js/gVenture.js
@@ -19,16 +19,20 @@ var config = {
 };
 //Pass our config to the Phaser game object
 var game = new Phaser.Game(config);
-
+var cursors;
+var graphics;
+var player;
 //Preloading image, sound, and any other assets that need to get loaded here
 function preload () {
+	this.isZoomed = false;
     //this.load.image('logo', 'assets/pics/logo.png');
 	this.load.image('BG', 'assets/pics/bg.png');
+	this.load.image('player', 'assets/pics/cursor.png');
 	this.load.image('leaves', 'assets/pics/logo.png');
 	/* End Image loading */
 	/*Spritesheets Below*/
-	this.load.spritesheet('leaves','assets/dude.png',
-		{ frameWidth: 32, frameHeight: 48 } //Gets the width and height ofe very frame to play
+	this.load.spritesheet('frg','assets/pics/frg.png',
+		{ frameWidth: 74, frameHeight: 59 } //Gets the width and height ofe very frame to play
     );
 	
 	/*End Spritesheets*/
@@ -43,23 +47,32 @@ function preload () {
 }
 //Actual creation/placement of our preloaded assets
 function create () {
+	window.addEventListener('resize', resize);//window.requestPointerLock();
+    resize();
+	gameCamera = this.cameras.main; //Get our camera object
+	//gameCamera.startFollow(game.input.mousePointer,false, 0.1, 0.1);
+	//gameCamera.setZoom(0.65);
+	this.cursors = this.input.keyboard.createCursorKeys();
+	
+	gameCamera.setViewport(0,0, 1920,1080);
+	this.cameras.main.setBounds(0,0, 4800, 2700, true); 
 	this.physics.world.setBounds(0,0, 4800, 2700, true, true, true, true); //Set bounds, first parameter will be top left x bound coordinate, second paramter the top left y bound, followed by width, then height
-	this.input.setDefaultCursor('url(assets/pics/cursor.png), pointer');
-	const gameCamera = this.cameras.main; //Get our camera object
+	//this.input.setDefaultplayer('url(assets/pics/player.png), pointer');
+	this.player = this.physics.add.sprite(50, 150, 'player').setDepth(1000);
+	this.player.setCollideWorldBounds();
 	//Camera's inputs are its x position (relative to top left corner), y position, camera width, and camera height
-	gameCamera.startFollow(game.input.mousePointer,false, 0.1, 0.1, game.input.mousePointer.x/2+120, game.input.mousePointer.y-120);
+	gameCamera.startFollow(this.player,false, 0.1, 0.1);
 	// startFollow tells the camera to follow (OBJECTNAME, roundPxtoNearestInt, trackSpeedX, trackSpeedY, offsetX,offsetY)
-
-    const tempBG = this.add.image(-1200, -600, 'BG');//Need to do this as it offsets a bit otherwise?
+	const tempBG = this.add.image(0,0, 'BG');
 	tempBG.setOrigin(0,0).setDepth(0);//Sets anchor to upper left (Needed to anchor bg to Phaser's origin pt 0,0
 	//set Depth helps ensure the BG stays on the bottom so everything else loads on top
 
 	initializeAudio();
 	initializeAnim();
 	
-	var leaves = this.add.sprite(0, 0, 'leaves').setDepth(2);
-	var leavesShadow = this.add.sprite(leaves.x+10,leaves.y-10,'leaves').setDepth(1);
-	//leavesShadow.tint = 0xFFFF00;
+	var leaves = this.add.sprite(0, 0, 'leaves').setDepth(2)
+	//leaves.setScale(2);
+	var leavesShadow = this.add.sprite(leaves.x+10,leaves.y+10,'leaves').setDepth(1);
 	leavesShadow.tint = 0x000000;
 	leavesShadow.alpha = 0.5;
 	leavesShadow.setVisible(false);
@@ -73,22 +86,52 @@ function create () {
 		leaves.anims.play('leavesRustle');
 
 	  });
+	 if (this.cameras.main.deadzone) {
+		graphics = this.add.graphics().setScrollFactor(0);
+		graphics.strokeRect(200,200,this.cameras.main.deadzone.width,this.cameras.main.deadzone.height);
+	 }
+	 alert(this.player.getBounds());
 }
 
 function update() {
 	console.log(this.input.mousePointer.worldX + " px");//Mouse's world coordinate not updating so clicks, on hover, adn other such events wont work...
-	//Need to change way camera works potentially, or figure out way to get the mouses coordinate to update 
+	 /*if (game.input.activePointer.isDown) {
+		if (!this.isZoomed) {
+			this.cameras.main.setZoom(1.5);
+			this.cameras.main.centerOn(this.input.activePointer.x, this.input.activePointer.y);
+			this.isZoomed = true;
+		} else if (this.isZoomed) {
+			this.isZoomed = false;
+			this.cameras.main.setZoom(0.6);
+	}
+	} */
+	
+	this.player.setVelocity(0);
+	if (this.cursors.left.isDown) {
+		this.player.setVelocityX(-900);
+	} else if (this.cursors.right.isDown) {
+		this.player.setVelocityX(900);
+	}
+	
+	if (this.cursors.up.isDown) {
+		this.player.setVelocityY(-900);
+	} else if (this.cursors.down.isDown) {
+		this.player.setVelocityY(900);
+	}
 }
 
+function resize() {
+    var canvas = game.canvas, width = window.innerWidth * window.devicePixelRatio, height = window.innerHeight* window.devicePixelRatio;
+    var wratio = width / height, ratio = canvas.width / canvas.height;
 
-/*function interactWithObject(Sprite sprite) {
-	//switch case for dealing with objects based off of Tag more than likely?
-	switch (true) {
-		sprite = 0;
-		
-		
-	}
-}*/
+    if (wratio < ratio) {
+        canvas.style.width = width + "px";
+        canvas.style.height = (width / ratio) + "px";
+    } else {
+        canvas.style.width = (height * ratio) + "px";
+        canvas.style.height = height + "px";
+    }
+}
 
 function initializeAudio() { //Load audio, we need to randomly play some BG noise? Def want to play when player clicks on related obj
 	this.birds = game.sound.add('birds');//Needs to be attached to game object
@@ -103,8 +146,8 @@ function initializeAudio() { //Load audio, we need to randomly play some BG nois
 function initializeAnim() {
 	game.leavesRustle = game.anims.create({ //Literally just a setup for animations that will be called upon later...
 		key: 'leavesRustle', //The key is what we will call when playing said anims
-		frames: game.anims.generateFrameNumbers('leaves', {start:0, end:3 /*End is the end frame, so if theres 4 frames end is 3*/}), 
-		framerate:20,
-		repeat:0 //DOnt repeat
+		frames: game.anims.generateFrameNumbers('frg', {start:0, end:4 /*End is the end frame, so if theres 4 frames end is 3*/}), 
+		framerate:16,
+		repeat:-1 //DOnt repeat -1 = repeat, 0 no repeat, 1 plays twice?
 	});
 }
